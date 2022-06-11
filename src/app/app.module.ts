3efa5458125070cbbeb526e5d10686c51c9308c5
ANDRES RODRIGUEZ <elpoloxrodriguez@gmail.com>
2022-06-11 08:35:45 -0400

    Maqueta de Landing Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './contents/app/app.component';
+import { FooterComponent } from './contents/shared/footer/footer.component';
+import { CarouselComponent } from './contents/shared/carousel/carousel.component';
+import { SideMenuRightComponent } from './contents/shared/side-menu-right/side-menu-right.component';
+import { SubMenuBarComponent } from './contents/shared/sub-menu-bar/sub-menu-bar.component';
+import { SocialMediaComponent } from './contents/shared/social-media/social-media.component';
+import { ChatBotComponent } from './contents/shared/chat-bot/chat-bot.component';
+import { PreloaderComponent } from './contents/shared/preloader/preloader.component';
+import { BackToTopComponent } from './contents/shared/back-to-top/back-to-top.component';
+import { NewsComponent } from './contents/shared/news/news.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    CarouselComponent,
+    SideMenuRightComponent,
+    SubMenuBarComponent,
+    SocialMediaComponent,
+    ChatBotComponent,
+    PreloaderComponent,
+    BackToTopComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/contents/shared/sub-menu-bar/sub-menu-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sub-menu-bar',
+  templateUrl: './sub-menu-bar.component.html',
+  styleUrls: ['./sub-menu-bar.component.scss']
+})
+export class SubMenuBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
