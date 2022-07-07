47e951e549eda18098a8b29a514f4f099b7fbdb9
ANDRES RODRIGUEZ <elpoloxrodriguez@gmail.com>
2022-07-07 19:51:59 -0400

    Correcion de posiciones

--- src/app/contents/shared/system-online/system-online.component.ts
+++ src/app/contents/shared/system-online/system-online.component.ts
@@ -8,14 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class SystemOnlineComponent implements OnInit {
 
   listSystem = [
-    { img: 'assets/images/system-online/3.png' },
-    { img: 'assets/images/system-online/3.png' },
-    { img: 'assets/images/system-online/3.png' },
-    { img: 'assets/images/system-online/3.png' },
-    { img: 'assets/images/system-online/3.png' },
-    { img: 'assets/images/system-online/3.png' },
-    { img: 'assets/images/system-online/3.png' },
-    { img: 'assets/images/system-online/3.png' }
+    { img: 'assets/images/system-online/3.png', title: 'SINEA', url: 'http://sinea.inea.gob.ve/sinea'},
+    { img: 'assets/images/system-online/3.png', title: 'CENAVE', url: 'https://cenave.inea.gob.ve/'},
+    { img: 'assets/images/system-online/3.png', title: 'GENTE DE MAR', url: 'http://sinea.inea.gob.ve/certificados/'},
+    { img: 'assets/images/system-online/3.png', title: 'RECAUDACION', url: 'http://recaudacion.inea.gob.ve/sinea/'},
+    { img: 'assets/images/system-online/3.png', title: 'WEB VTS - INEA', url: 'http://inea.web-vts.com/INEA/login.aspx?ReturnUrl=%2finea%2fAView.aspx'},
+    { img: 'assets/images/system-online/3.png', title: 'BUQUES RENAVE', url: 'http://sinea.inea.gob.ve/consulta_buque/'},
+    { img: 'assets/images/system-online/3.png', title: 'BOLETIN METEOROLOGÍCO', url: '#'},
+    { img: 'assets/images/system-online/3.png', title: 'TARIFARIO', url: '#'},
+    { img: 'assets/images/system-online/3.png', title: 'MATRICULACION', url: 'http://www.inea.gob.ve/rematriculacion/'},
+    { img: 'assets/images/system-online/3.png', title: 'SALVOCONDUCTO DE MOVILIZACION', url: 'http://www.inea.gob.ve/salvoconducto/'},
+    { img: 'assets/images/system-online/3.png', title: 'SISTEMA ARMONIZADO', url: 'http://www.inea.gob.ve/reconocimiento/'},
+    { img: 'assets/images/system-online/3.png', title: 'DECLARACIONES INTERNACIONALES', url: 'http://www.inea.gob.ve/camtrip/'}
 ]
 
   constructor() { }
